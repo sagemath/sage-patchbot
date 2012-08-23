5a6eeb3ce8b03399ddeb07adf453f59f29710761
Robert Bradshaw <robertwb@gmail.com>
2012-08-23 02:11:17 -0700

    spkg creation and installation

--- make-spkg.sh
+++ make-spkg.sh
@@ -1,34 +1,54 @@
 #/bin/bash
 
-VERSION=HEAD
+if [ $# -ge 1 ]; then
+    VERSION=$1
+else
+    VERSION="UNCOMMITTED"
+fi
+
+if [ ! -e .git ]; then
+    echo "Must be called from the git repository."
+    echo
+    echo "git clone git://github.com/robertwb/sage-patchbot.git"
+    exit 1
+fi
 
 rm -rf workspace-*
 TMP=$(mktemp -d workspace-XXXXXX)
 ORIGINAL=$(pwd)
 
-if [ ! -e .git ]
-then
-    echo "make-spkg only works from within a git repo"
-    echo "please run"
-    echo "    git clone git://github.com/robertwb/sage-patchbot.git upstream"
-    exit 1
+git diff --stat
+
+if [ "$VERSION" == "UNCOMMITTED" ]; then
+    git diff > $TMP/uncommitted.patch
+    HEAD=$(git rev-parse HEAD)
+else
+    status=$(sage -hg status)
+    if [ -n "$status" ]; then
+	echo "Uncommitted hg changes."
+	echo "$status"
+	exit 1
+    fi
+    HEAD=$(git rev-parse $VERSION)
+    if [ "$?" -ne "0" ]; then
+	echo "Unknown tag or commit: $VERSION"
+	exit 1
+    fi
 fi
 
-status=$(sage -hg status)
-if [ -n "$status" ]; then
-    echo "Uncommitted changes."
-    echo "$status"
-    exit 1
-fi
-
+# Clone the repo.
 cd $TMP
-sage -hg clone $ORIGINAL patchbot-$VERSION
+git clone $ORIGINAL patchbot-$VERSION
+cd patchbot-$VERSION
+if [ -e "../uncommitted.patch" ]; then
+    patch -p1 < ../uncommitted.patch
+fi
 
-git clone $ORIGINAL/upstream patchbot-$VERSION/src
-cd patchbot-$VERSION/src
-git checkout -q $VERSION
+# Format as an spkg.
 rm -rf .git
-cd ../..
+cp -r $ORIGINAL/.hg .
+mv README.txt src
+cd ..
 
 sage -spkg patchbot-$VERSION
 cp patchbot-$VERSION.spkg $ORIGINAL
