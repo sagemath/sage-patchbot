4e65f7494b6142476e37de1ba21b10899fcc251b
Robert Bradshaw <robertwb@gmail.com>
2013-05-20 22:23:14 -0700

    Only check git if repo exists.

--- src/version.py
+++ src/version.py
@@ -4,10 +4,11 @@ def get_version():
     src_dir = os.path.dirname(os.path.abspath(__file__))
     top = os.path.dirname(src_dir)
     git_dir = os.path.join(top, '.git')
-    try:
-        return subprocess.check_output(['git', '--work-tree=' + top, '--git-dir=' + git_dir, 'describe', '--tags', '--dirty'])
-    except:
-        pass
+    if os.path.exists(git_dir):
+        try:
+            return subprocess.check_output(['git', '--work-tree=' + top, '--git-dir=' + git_dir, 'describe', '--tags', '--dirty'])
+        except:
+            pass
     version_file = os.path.join(src_dir, 'version.txt')
     if os.path.exists(version_file):
         return open(version_file).read().strip()
