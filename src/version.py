2aa5e6a60b196f5ac5c38e6035de19a74a5526f3
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2015-03-25 22:35:21 +0100

    cleanup of version.py and util.py, with more doc and pep8 formatting

--- src/version.py
+++ src/version.py
@@ -1,19 +1,30 @@
-import os, subprocess
+import os
+import subprocess
+
 
 def get_version():
+    """
+    Get the version of sage-patchbot.
+
+    First try using git tags then using the version.txt file.
+
+    The expected result is a string of the shape '2.2' or '2.3.rc0'.
+    """
     src_dir = os.path.dirname(os.path.abspath(__file__))
     top = os.path.dirname(src_dir)
     git_dir = os.path.join(top, '.git')
     if os.path.exists(git_dir):
         try:
-            return subprocess.check_output(['git', '--work-tree=' + top, '--git-dir=' + git_dir, 'describe', '--tags', '--dirty'])
+            return subprocess.check_output(['git', '--work-tree=' + top,
+                                            '--git-dir=' + git_dir,
+                                            'describe', '--tags', '--dirty'])
         except:
             pass
     version_file = os.path.join(src_dir, 'version.txt')
     if os.path.exists(version_file):
         return open(version_file).read().strip()
-    else:
-        return 'unknown'
+    # failure
+    return 'unknown'
 
 if __name__ == '__main__':
     print get_version()
