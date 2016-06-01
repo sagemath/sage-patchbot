6936c30e6094748b36ba6e722d1dcc4c97107a3a
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2016-06-01 17:48:23 +0200

    changing name of python package to patchbot

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def find_version(*file_paths):
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
 
-setup(name='sage-patchbot',
+setup(name='patchbot',
       version=find_version('sage_patchbot', 'version.py'),
       description='bot for automatic test of sagemath trac tickets',
       url='https://github.com/robertwb/sage-patchbot',
