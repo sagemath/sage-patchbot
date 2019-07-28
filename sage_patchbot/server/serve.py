aac6e239ad1ac1a6f4660329db245272e4b6e0d0
Frédéric Chapoton <chapoton@unistra.fr>
2019-07-28 09:01:07 +0200

    antoher fix 'r'

--- sage_patchbot/server/serve.py
+++ sage_patchbot/server/serve.py
@@ -679,7 +679,7 @@ def get_log(log):
     if not db.logs.exists(path):
         data = "No such log!"
     else:
-        with bz2.open(db.logs.get(path)) as f:
+        with bz2.open(db.logs.get(path), 'r') as f:
             data = f.read()
     if 'plugin' in request.args:
         plugin = request.args.get('plugin')
@@ -688,7 +688,7 @@ def get_log(log):
             header = data[:data.find('\n')]
             base = request.args.get('base')
             ticket_id = request.args.get('ticket')
-            with bz2.open(db.logs.get(request.args.get('diff'))) as f:
+            with bz2.open(db.logs.get(request.args.get('diff')), 'r') as f:
                 base_data = f.read()
             base_data = extract_plugin_log(base_data, plugin)
             diff = difflib.unified_diff(base_data.split('\n'), data.split('\n'), base, "%s + #%s" % (base, ticket_id), n=0)
