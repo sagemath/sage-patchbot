33cb310e07bd878e8a0766aa6e29a1973c4021f2
Frédéric Chapoton <chapoton@unistra.fr>
2020-07-06 09:20:13 +0200

    tweak in deciding if a ticket is safe

--- sage_patchbot/trac.py
+++ sage_patchbot/trac.py
@@ -256,7 +256,8 @@ def inplace_safe():
                 file.startswith("src/doc") or
                 file.startswith("build/pkgs") or
                 file in ("src/setup.py", "src/module_list.py",
-                         "README.txt", ".gitignore",
+                         ".lgtm.yml", "tox.ini", "condarc.yml",
+                         "README.md", ".gitignore",
                          "VERSION.txt", "src/bin/sage-banner",
                          "src/bin/sage-version.sh")):
             continue
