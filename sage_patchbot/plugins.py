0bf08a0a68c7aa3e1b442569128fd17104435f98
Frédéric Chapoton <chapoton@unistra.fr>
2021-08-25 19:54:06 +0200

    activate E711 and E712 pycodestyle

--- sage_patchbot/plugins.py
+++ sage_patchbot/plugins.py
@@ -328,7 +328,7 @@ def pycodestyle(ticket, **kwds):
     """
     run ``pycodestyle --select=...`` on the modified .py files
 
-    with --select=W605, E401, E701, E702
+    with --select=W605, E401, E701, E702, E711, E712
 
     see http://pycodestyle.pycqa.org/en/latest/intro.html#error-codes
 
@@ -340,7 +340,8 @@ def pycodestyle(ticket, **kwds):
     changed_files = list(subprocess.Popen(['git', 'diff', '--name-only', 'patchbot/base..patchbot/ticket_merged'], stdout=subprocess.PIPE).stdout)
     changed_files = [f.decode('utf8').strip("\n") for f in changed_files]
 
-    style = StyleGuide(select=['W605', 'E401', 'E701', 'E702'])
+    style = StyleGuide(select=['W605', 'E401', 'E701', 'E702',
+                               'E711', 'E712'])
     errors = 0
     for a_file in changed_files:
         if os.path.exists(a_file) and os.path.splitext(a_file)[1] == '.py':
